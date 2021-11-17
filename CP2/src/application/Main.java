f9a1aea96c2afb1ee6e73b66ff818616193b05dd
ImaMature <88884623+ImaMature@users.noreply.github.com>
2021-11-18 01:28:25 +0900

    폰트넣어봄

--- CP2/src/application/Main.java
+++ CP2/src/application/Main.java
@@ -6,6 +6,7 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 
 
 public class Main extends Application {
@@ -14,10 +15,16 @@ public class Main extends Application {
 		try {
 			Parent parent = FXMLLoader.load(getClass().getResource("/View/MainPage.fxml"));
 			Scene scene = new Scene(parent);
-			stage.setScene(scene);
-			stage.setResizable(false);
-			stage.setTitle("Gazua!");
-			stage.show();
+			// 폰트 로드 
+
+			Font.loadFont( getClass().getResourceAsStream("esamanru Medium.ttf"), 20);
+
+			// 외부 스타일시트 적용 
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+				stage.setScene(scene);
+				stage.setResizable(false);
+				stage.setTitle("Gazua!");
+				stage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
